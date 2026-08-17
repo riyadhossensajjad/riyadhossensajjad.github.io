d145171ba818b02902c37aa0e5a113776905d4a6
Md Riyad Hossen Sajjad <riyadhossensajjad@gmail.com>
2026-08-17 23:10:47 +0600

    Update GPA & SQL Icon

--- js/data.js
+++ js/data.js
@@ -50,7 +50,7 @@ const EDUCATION = [
     period: "2028",
     degree: "B.Sc. in Computer Science and Engineering",
     gpa: "5th Semester",
-    school: "University of Asia Pacific"
+    school: "University of Asia Pacific, Dhaka"
   }
 ];
 
@@ -80,7 +80,12 @@ const PROGRAMMING_SKILLS = [
   { 
     name: "SQL", 
     level: 65, 
-    icon: "https://raw.githubusercontent.com/tabler/tabler-icons/main/icons/outline/database.svg" 
+    icon: true,
+    svg: `<svg viewBox="0 0 24 24" width="28" height="28" fill="none" stroke="#485563" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round">
+            <ellipse cx="12" cy="5" rx="9" ry="3"></ellipse>
+            <path d="M21 12c0 1.66-4 3-9 3s-9-1.34-9-3"></path>
+            <path d="M3 5v14c0 1.66 4 3 9 3s9-1.34 9-3V5"></path>
+          </svg>`
   },
   { 
     name: "Java", 
